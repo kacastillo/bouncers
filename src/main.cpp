a95f9d3235d7a131339506cdc649e0d58a0b9f98
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-02-17 16:20:28 +0000

    Bouncing logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,51 @@
 #include <bn_core.h>
 #include <bn_backdrop.h>
-#include <bn_color.h>
 #include <bn_keypad.h>
+#include <bn_sprite_ptr.h>
+#include <bn_display.h>
+
+#include "bn_sprite_items_dot.h"
+
+static constexpr int HALF_SCREEN_WIDTH = bn::display::width() / 2;
+static constexpr int HALF_SCREEN_HEIGHT = bn::display::height() / 2; 
 
 int main() {
     bn::core::init();
 
-    bn::backdrop::set_color(bn::color(31, 0, 0));
+    bn::sprite_ptr bouncer = bn::sprite_items::dot.create_sprite(20, 50);
+
+    bn::fixed dx = -2.5;
+    bn::fixed dy = 3.1;
 
     while(true) {
-        if(bn::keypad::a_pressed()) {
-            bn::backdrop::set_color(bn::color(0, 0, 30));
+
+        bn::fixed x = bouncer.x();
+        bn::fixed y = bouncer.y();
+
+        x += dx;
+        y += dy;
+
+        if(x > HALF_SCREEN_WIDTH) {
+            x = HALF_SCREEN_WIDTH;
+            dx *=-1;
         }
-        if(bn::keypad::b_pressed()) {
-            bn::backdrop::set_color(bn::color(0, 31, 0));
+        if(x < -HALF_SCREEN_WIDTH) {
+            x = -HALF_SCREEN_WIDTH;
+            dx *= -1;
         }
+
+        if(y > HALF_SCREEN_HEIGHT) {
+            y = HALF_SCREEN_HEIGHT;
+            dy *= -1;
+        }
+        if(y < -HALF_SCREEN_HEIGHT) {
+            y = -HALF_SCREEN_HEIGHT;
+            dy *= -1;
+        }
+
+        bouncer.set_x(x);
+        bouncer.set_y(y);
+
         bn::core::update();
     }
 }
